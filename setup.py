e2094ea9339085c5a334580db628b36d013a82b1
Monika Scholz <monika.k.scholz@gmail.com>
2019-11-21 11:47:10 +0100

    intial package structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='pypeline',
+      version='0.1',
+      description='Analysis of wholebrain imaging data in freely behaving C. elegans',
+      author='M.Scholz, F. Randi et. al',
+      author_email='mscholz@princeton.edu',
+      packages=['pypeline'],
+     )
