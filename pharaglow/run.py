fd80fd76f89aeb8482ab14fb15a2a1ca4ceacb77
Monika Scholz <monika.k.scholz@gmail.com>
2020-01-08 10:13:32 +0100

    remove unneccessary dependencies

--- pharaglow/run.py
+++ pharaglow/run.py
@@ -1,10 +1,13 @@
-import matplotlib.pylab as plt
+#!/usr/bin/env python
+
+"""run.py: run pharaglow analysis by inplace modifying pandas dataframes."""
+
 import numpy as np
 from skimage.io import imread
 from skimage import util
 import json
-
 import pandas as pd
+
 import pharaglow.features as pg
 import pharaglow.tracking as pgt
 
@@ -19,35 +22,45 @@ def runPharaglowSkel(im):
     return mask, ptsX, ptsY
 
 
-def runPharaglowCL(mask, ptsX, ptsY, nPts = 25):
+def runPharaglowCL(mask, ptsX, ptsY, nPts = 100):
     # getting centerline and widths along midline
     poptX, poptY = pg.fitSkeleton(ptsX, ptsY)
     contour = pg.morphologicalPharynxContour(mask, scale = 4, smoothing=2)
     xstart, xend = pg.cropcenterline(poptX, poptY, contour, nP = len(ptsX))
-    # make sure centerline isn't too short if something goes wring
+    # extend a bit to include all bulbs
+    #l = np.abs(xstart-xend)
+    #xstart -= 0.05*l
+    #xend += 0.05*l
+    # make sure centerline isn't too short if something goes wrong
     if np.abs(xstart-xend) < 0.5*len(ptsX):
         xstart, xend = 0, len(ptsX)
     xs = np.linspace(xstart, xend, nPts)
     cl = pg.centerline(poptX, poptY, xs)
     dCl = pg.normalVecCl(poptX, poptY, xs)
     widths = pg.widthPharynx(cl, contour, dCl)
-
+    if (np.argmax(pg.scalarWidth(widths)) - 0.5*len(widths)) <= 0:
+        xtmp = xstart
+        xstart = xend
+        xend = xtmp
+        cl = cl[::-1]
+        dCl = dCl[::-1]
+        widths = widths[::-1]
     return poptX, poptY, xstart, xend, cl, dCl, widths, contour
 
 
 def runPharaglowKymo(im, cl, widths, **kwargs):
     kymo = pg.intensityAlongCenterline(im, cl, **kwargs)
-    kymoWeighted = pg.intensityAlongCenterline(im, cl, width = pg.scalarWidth(widths))
+    kymoWeighted = pg.intensityAlongCenterline(im, cl, width = pg.scalarWidth(widths))[:,0]
     return kymo, kymoWeighted
 
 
-def runPharaglowImg(im, xstart, xend, poptX, poptY, width):
+def runPharaglowImg(im, xstart, xend, poptX, poptY, width, npts):
     # make sure image is float
     im = util.img_as_float64(im)
     #local derivative, can enhance contrast
     gradientImage = pg.gradientPharynx(im)
     # straightened image
-    straightIm = pg.straightenPharynx(im, xstart, xend, poptX, poptY, width=width)
+    straightIm = pg.straightenPharynx(im, xstart, xend, poptX, poptY, width=width, nPts = npts)
     return gradientImage, straightIm
 
 
@@ -59,23 +72,24 @@ def pharynxorientation(df):
     df['Similarity'] = False
     for particleID in df['particle'].unique():
         mask = df['particle']==particleID
-        sample = df[mask]['StraightKymo'].iloc[0]
+        sample = df[mask]['StraightKymo'].iloc[30]
         # let's make sure the sample is anterior to posterior
         #if np.mean(sample[:len(sample//2)])<np.mean(sample[len(sample//2):]):
         #    sample = sample[::-1]
         df['Similarity'] = np.where(mask, df.apply(\
-          lambda row: np.sum((row['StraightKymo']-sample)**2)>np.sum((row['StraightKymo']-sample[::-1])**2), axis=1)\
+          lambda row: np.sum((row['StraightKymo']-sample)**2)<np.sum((row['StraightKymo']-sample[::-1])**2), axis=1)\
                                     , df['Similarity'])
 
     # now flip the orientation where the sample is upside down
     for key in ['SkeletonX', 'SkeletonY', 'Centerline', 'dCl', 'Widths', 'Kymo',\
-           'WeightedKymo', 'StraightKymo', 'Straightened']:
+           'WeightedKymo', 'StraightKymo', 'Straightened', 'KymoGrad', 'WeightedKymoGrad']:
         df[key] = df.apply(lambda row: row[key] if row['Similarity'] else row[key][::-1], axis=1)
     # Flip the start coordinates
     #df.update(df.loc[df['Similarity']].rename({'Xstart': 'Xend', 'Xend': 'Xstart'}, axis=1))
     df['Xtmp'] = df['Xstart']
     df['Xstart'] = df.apply(lambda row: row['Xstart'] if row['Similarity'] else row['Xend'], axis=1)
     df['Xend'] = df.apply(lambda row: row['Xend'] if row['Similarity'] else row['Xtmp'], axis=1)
+    return df
 
 
 def runPharaglowOnStack(df, param):
@@ -90,12 +104,15 @@ def runPharaglowOnStack(df, param):
     df[['Gradient', 'Straightened']] = df.apply(\
         lambda row: pd.Series(runPharaglowImg(row['image'], row['Xstart'], row['Xend'],\
                                               row['ParX'], row['ParY'], param['widthStraight'],\
-                                             )), axis=1)
+                                             param['nPts'])), axis=1)
     # run kymographs
     df[['Kymo', 'WeightedKymo']] = df.apply(\
         lambda row: pd.Series(runPharaglowKymo(row['image'], row['Centerline'], row['Widths'], linewidth = 2)), axis=1)
+    # run kymographs
+    df[['KymoGrad', 'WeightedKymoGrad']] = df.apply(\
+        lambda row: pd.Series(runPharaglowKymo(row['Gradient'], row['Centerline'], row['Widths'], linewidth = 5)), axis=1)
     ## clean orientation
-    pharynxorientation(df)
+    df = pharynxorientation(df)
     return df
 
 
