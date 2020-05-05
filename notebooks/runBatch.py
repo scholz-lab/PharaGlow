51b072881eed159f5bba5801cda33f2c2556b116
Monika Scholz <monika.k.scholz@gmail.com>
2020-05-05 21:09:12 +0200

    ignore hidden directories when batch analyzing

--- notebooks/runBatch.py
+++ notebooks/runBatch.py
@@ -8,17 +8,17 @@ dataFolder = "/media/scholz_la/hd2/Nicolina/Raw_videos/10x_INF100_test"
 # create a dictionary of parameters
 for subfolder in [f.path for f in os.scandir(dataFolder) if f.is_dir()]:
     movie = subfolder.split('/')[-1]
-    
-    pars = { 'parameterfile': parameterfile,
+    if not movie.startswith('.'):
+        pars = { 'parameterfile': parameterfile,
             'inPath': subfolder,
             'outPath': dataFolder,
             'lawnPath': lawnPath,
             'movie': movie,
             'nWorkers': 5,
-    }
-    
-    pm.execute_notebook(
-       '/home/nzjacic/Desktop/Code/PharaGlow/notebooks/BatchRunTemplate.ipynb',
-       os.path.join(dataFolder, 'out_{movie}.ipynb'),
-       parameters=pars
-   )
+        }
+        print('Analyzing {}. Output can be watched live in'.format(movie), os.path.join(dataFolder, 'out_{}.ipynb'.format(movie)))
+        pm.execute_notebook(
+           '/home/nzjacic/Desktop/Code/PharaGlow/notebooks/BatchRunTemplate.ipynb',
+           os.path.join(dataFolder, 'out_{movie}.ipynb'),
+           parameters=pars
+       )
