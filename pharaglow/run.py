877241cef1422f2e0adf66a3d6797a082ccd3615
Monika Scholz <mscholz@caesar.de>
2021-04-13 20:43:32 +0200

    major rewrite separating images and dataframes.

--- pharaglow/run.py
+++ pharaglow/run.py
@@ -13,19 +13,17 @@ import pharaglow.tracking as pgt
 import pharaglow.util as pgu
 
 
-def runPharaglowSkel(im, length):
+def runPharaglowSkel(im):
     """create a centerline of the object in the image by binarizing, skeletonizing and sorting centerline points.
         Inputs:
-            im: image, unraveled
+            im: image
             length: length of one axis of the image
         Outputs:
-            mask: binary of iage, unraveled
+            mask: binary of image, unraveled
             ptsX: coordinates of centerline along X
             ptsY: coordinates of centerline along Y
     """
-    # preprocessing image
-    im = np.array(im, dtype=int)
-    im = pgu.unravelImages(im, length)
+    
     mask = pg.thresholdPharynx(im)
     skel = pg.skeletonPharynx(mask)
     order = pg.sortSkeleton(skel)
@@ -74,23 +72,21 @@ def runPharaglowCL(mask, ptsX, ptsY, length, nPts = 100):
     return poptX, poptY, xstart, xend, cl, dCl, widths, contour
 
 
-def runPharaglowKymo(im, cl, widths, length, **kwargs):
+def runPharaglowKymo(im, cl, widths, **kwargs):
     """Use the centerline to extract intensity along this line from an image.
        Inputs:
-            im: image, unraveled
-            length: length of one axis of the image
+            im: image
             cl: (n,2) list of centerline coordinates in image space.
             widths: scalar width along centerline (n,2)
         Outputs:
             intensity (N,): array of pixel intensities
     """
-    im = np.array(im, dtype = int)
-    im = pgu.unravelImages(im, length)
+    
     #kymoWeighted = pg.intensityAlongCenterline(im, cl, width = pg.scalarWidth(widths))[:,0]
     return [pg.intensityAlongCenterline(im, cl, **kwargs)]
 
 
-def runPharaglowImg(im, xstart, xend, poptX, poptY, width, npts, length):
+def runPharaglowImg(im, xstart, xend, poptX, poptY, width, npts):
     """Obtain the straightened version and gradient of the input image.
         Inputs:
             im: an image
@@ -102,43 +98,32 @@ def runPharaglowImg(im, xstart, xend, poptX, poptY, width, npts, length):
             straightIm:  (nPts, width) array of image intensity
             
     """
-    # make sure image is float
-    im = np.array(im, dtype = 'int')
-    im = pgu.unravelImages(im, length)
-    #im = np.array(im)
-    #im = util.img_as_float64(im)
+    
     #local derivative, can enhance contrast
     gradientImage = pg.gradientPharynx(im)
-
     # straightened image
     straightIm = pg.straightenPharynx(im, xstart, xend, poptX, poptY, width=width, nPts = npts)
     return gradientImage, straightIm
 
 
 def pharynxorientation(df):
-    """A Get the orientation from the minimal trajectory of the start and end points..."""
+    """A Get the orientation from the minimal trajectory of the start and end points for a single trajectory."""
     df.loc[:,'StraightKymo'] = df.apply(
     lambda row: np.mean(row['Straightened'], axis = 1), axis=1)
     df['Similarity'] = False
-    for particleID in df['particle'].unique():
-
-        mask = df['particle']==particleID
-        sample = df[mask]['StraightKymo'].mean()
-       
-        # let's make sure the sample is anterior to posterior
-        if np.mean(sample[:len(sample//2)])>np.mean(sample[len(sample//2):]):
-            sample = sample[::-1]
-        # this uses the intenisty profile - sometimes goes wrong
-        df['Similarity'] = np.where(mask, df.apply(\
-          lambda row: np.sum((row['StraightKymo']-sample)**2)<np.sum((row['StraightKymo']-sample[::-1])**2), axis=1)\
-                                    , df['Similarity'])
+    sample = df['StraightKymo'].mean()
+    # let's make sure the sample is anterior to posterior
+    if np.mean(sample[:len(sample//2)])>np.mean(sample[len(sample//2):]):
+        sample = sample[::-1]
+    # this uses the intenisty profile - sometimes goes wrong
+    df['Similarity'] = df.apply(\
+        lambda row: np.sum((row['StraightKymo']-sample)**2)<np.sum((row['StraightKymo']-sample[::-1])**2), axis=1)
     # now flip the orientation where the sample is upside down
     for key in ['SkeletonX', 'SkeletonY', 'Centerline', 'dCl', 'Widths', 'Kymo',\
            'StraightKymo', 'Straightened', 'KymoGrad']:
         if key in df.columns:
             df[key] = df.apply(lambda row: row[key] if row['Similarity'] else row[key][::-1], axis=1)
     # Flip the start coordinates
-    
     if set(['Xstart', 'Xend']).issubset(df.columns):
         df['Xtmp'] = df['Xstart']
         df['Xstart'] = df.apply(lambda row: row['Xstart'] if row['Similarity'] else row['Xend'], axis=1)
@@ -146,29 +131,63 @@ def pharynxorientation(df):
     return df
 
 
-def runPharaglowOnStack(df, param, run_all = True):
-    """runs the whole pharaglow toolset on a dataframe. Can be linked or unliked before. only the last step depends on having a particle ID."""
-    # run image analysis on all rows of the dataframe
-    df[['Mask', 'SkeletonX', 'SkeletonY']] = df.apply(\
-        lambda row: pd.Series(runPharaglowSkel(row['image'], row['shapeX'])), axis=1)
-    # run centerline fitting
-    df[['ParX', 'ParY', 'Xstart', 'Xend', 'Centerline', 'dCl', 'Widths', 'Contour']] = df.apply(\
-        lambda row: pd.Series(runPharaglowCL(row['Mask'],row['SkeletonX'], row['SkeletonY'], row['shapeX'])), axis=1)
-    # run image operations
-    df[['Gradient', 'Straightened']] = df.apply(\
-        lambda row: pd.Series(runPharaglowImg(row['image'], row['Xstart'], row['Xend'],\
-                                              row['ParX'], row['ParY'], param['widthStraight'],\
-                                             param['nPts'], row['shapeX'])), axis=1)
+def runPharaglowOnImage(image, framenumber, params, **kwargs):
+    """"run pharaglow-specific image analysis on a single image."""
+    if 'run_all' in kwargs.keys():
+        run_all = kwargs['run_all']
+    else:
+        run_all = False
+    colnames = ['Mask', 'SkeletonX', 'SkeletonY','ParX', 'ParY', 'Xstart', 'Xend', 'Centerline', 'dCl', 'Widths', \
+        'Contour','Gradient', 'Straightened', 'Kymo', 'KymoGrad']
+    
+    # skeletonize
+    mask, skelX, skelY = runPharaglowSkel(image)
+    #centerline fit
+    parX, parY, xstart, xend, cl, dCl, widths, contour = runPharaglowCL(mask,skelX, skelY, params['length'])
+    # image transformation operations
+    grad, straightened = runPharaglowImg(image, xstart,xend,\
+                                            parX, parY, params['widthStraight'],\
+                                            params['nPts'])
+    results = [mask, skelX, skelY, parX, parY, xstart, xend, cl, dCl, widths, contour, grad, straightened]
     if run_all:
         # run kymographs
-        df[['Kymo']] = df.apply(\
-            lambda row: pd.Series(runPharaglowKymo(row['image'], row['Centerline'], row['Widths'], row['shapeX'],linewidth = param['linewidth'])), axis=1)
+        kymo= runPharaglowKymo(image, cl, widths, linewidth = params['linewidth'])
         # run kymographs
-        df[['KymoGrad']] = df.apply(\
-            lambda row: pd.Series(runPharaglowKymo(row['Gradient'], row['Centerline'], row['Widths'], row['shapeX'], linewidth = param['linewidth'])), axis=1)
-    # extract pumping metric
-    df[['pumps']] = df.apply(\
-        lambda row: pd.Series(pg.extractPump(row['Straightened'])), axis=1)
-    return df
+        kymograd = runPharaglowKymo(grad, cl, widths, linewidth = params['linewidth'])
+        results.append(kymo, kymograd)
+    data = {}
+    for col, res in zip(colnames,results):
+        data[col] = res
+    df = pd.DataFrame([data], dtype = 'object')
+    df['frame'] = framenumber
+    return df, 
 
+
+# def runPharaglowOnStack(image, index, param, run_all = True):
+#     """runs the whole pharaglow toolset on an image stack."""
+#     colnames = ['Mask', 'SkeletonX', 'SkeletonY','ParX', 'ParY', 'Xstart', 'Xend', 'Centerline', 'dCl', 'Widths', \
+#         'Contour','Gradient', 'Straightened', 'Kymo', 'KymoGrad']
+#     data = []
+#     df = pd.DataFrame(dtype = 'object')
+#     for idx in index:
+#         image = images[idx]
+#         if np.sum(image)==0:
+#             df.drop(idx)
+#             data.append(runPharaglowOnImage(image, param, run_all))
+#     if run_all:
+#         df[[colnames]] = data
+#     else:
+#         df[[colnames[:-2]]] = data
+#     df.index = index
+#     # extract pumping metric
+#     df[['pumps']] = df.apply(\
+#         lambda row: pd.Series(pg.extractPump(row['Straightened'])), axis=1)
+#     return df, images
+
+
+def parallel_pharaglow_run(args, **kwargs):
+    """define a worker function for parallelization."""
+    return runPharaglowOnImage(*args, **kwargs)
+
+        
 
