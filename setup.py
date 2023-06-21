452346bd6eab8cac75b8d513f5acfa83e0aa167e
Omar Valerio <omar.valerio@gmail.com>
2023-06-21 09:33:30 +0200

    pharaglow pip build & dependencies using setuptools.

--- setup.py
+++ setup.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+#from distutils.core import setup
 import pharaglow
+from setuptools import setup
 
-setup(name='pharaglow',
-      version=pharaglow.__version__,
-      description='a toolset to analyze videos of foraging animals. ',
-      author='M.Scholz',
-      author_email='monika.k.scholz@gmail.com',
-      packages=['pharaglow'],
-     )
+if __name__ == "__main__":
+      setup(name='pharaglow',
+            version=pharaglow.__version__,
+            description='a toolset to analyze videos of foraging animals. ',
+            author='M.Scholz',
+            author_email='monika.k.scholz@gmail.com',
+            packages=['pharaglow'],
+            install_requires=[
+                  "numpy",
+                  "scipy",
+                  "pyampd",
+                  "pandas",
+                  "pims",
+                  "scikit-image",
+                  "trackpy",
+            ],
+      )
